cf62531cd4cf50e513bb71284480f0fed72774d6
GlamDead <nikita.ivshin@bravedevelopers.com>
2020-10-06 13:38:43 +0400

    Updates

---
+++ precache-manifest.366ffe2ff5b127966034f3c0e8b98492.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fea4287e3a5e3168b476119ecb463019",
+    "url": "/cat/index.html"
+  },
+  {
+    "revision": "ac65f6a078552e824a74",
+    "url": "/cat/static/js/2.4d9ea4cc.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/cat/static/js/2.4d9ea4cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ab8c6f0e25d77a1984a",
+    "url": "/cat/static/js/main.f685ec4b.chunk.js"
+  },
+  {
+    "revision": "7a7122893e77f7e05579",
+    "url": "/cat/static/js/runtime-main.8410d884.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.320469aa898a5706c482b9de3bf43c35.js"
+  "/cat/precache-manifest.366ffe2ff5b127966034f3c0e8b98492.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/cat/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
