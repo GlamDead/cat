9788d0fab86fb75ec99d57bcdee915727dece88a
GlamDead <nikita.ivshin@bravedevelopers.com>
2020-10-06 15:31:18 +0400

    Updates

---
+++ precache-manifest.f99816eff1ef24d0f656d86ea0402f04.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "587b5eee821a39d9958da22bb7027931",
+    "url": "/cat/index.html"
+  },
+  {
+    "revision": "ee9d0a44c14b9e80164d",
+    "url": "/cat/static/js/2.d8ef116e.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/cat/static/js/2.d8ef116e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d831004d5f1db8b3539",
+    "url": "/cat/static/js/main.6e20abb8.chunk.js"
+  },
+  {
+    "revision": "7a7122893e77f7e05579",
+    "url": "/cat/static/js/runtime-main.8410d884.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/cat/precache-manifest.e516304d745f430120523746cf915502.js"
+  "/cat/precache-manifest.f99816eff1ef24d0f656d86ea0402f04.js"
 );
 
 self.addEventListener('message', (event) => {
